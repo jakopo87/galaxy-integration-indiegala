30eee4fc7a6fa32657130a1904c78f178f634419
Jacopo Galati <jacopo.galati@outlook.com>
2021-07-02 14:15:42 +0200

    kill current process if present

--- deploy.ps1
+++ deploy.ps1
@@ -2,3 +2,9 @@ $pluginFolder = New-Item -ItemType Directory -Path ($env:LOCALAPPDATA + "\GOG.co
 Copy-Item -Force -Recurse -Path .\src\* -Destination $pluginFolder
 
 pip install -r .\requirements.txt -t $pluginFolder --implementation cp --python-version 37 --only-binary=:all:
+
+# Kill current process
+$processId = (Get-CimInstance -ClassName Win32_Process -Filter "name = 'python.exe' AND commandline LIKE '%indiegala%'").processid
+if ($null -ne $processId ) {
+    Stop-Process $processId
+}
